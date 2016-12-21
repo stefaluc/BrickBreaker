768cff4e111310ed1350d6fb370447798c43aa36
arnav-s <sharma55@wisc.edu>
2016-12-20 23:17:36 -0600

    Update Ball.hpp

--- Ball.hpp
+++ Ball.hpp
@@ -22,7 +22,9 @@ private:
     int yCoord;
     QImage image;
     QRect rectangle;
-    static const int INITIAL_X = 155;
-    static const int INITIAL_Y = 375;
-    static const int RIGHT_EDGE = 300;
+    static const int Initial_X = 155;
+    static const int Initial_Y = 375;
+    static const int Right_Wall = 300; 
+    static const int Left_Wall = 0; 
+    static const int Top_Wall = 0;
 };
