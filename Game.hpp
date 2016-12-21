f12ecfa87fb2415c4369f9597c60d28da8b37360
arnav-s <sharma55@wisc.edu>
2016-12-20 22:41:01 -0600

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -16,24 +16,25 @@ protected:
     void keyPressEvent(QKeyEvent *);
     void keyReleaseEvent(QKeyEvent *);
     void drawObjects(QPainter *);
-    void finishGame(QPainter *, QString);
+    void endGame(QPainter *, QString);
     void moveObjects();
     void startGame();
     void pauseGame();
-    void stopGame();
-    void victory();
-    void checkCollision();
+    void endGame();
+    void Win();
+    void Physics();
 
 private:
     int x;
-    int timerId;
-    static const int NUM_BRICKS = 30;
+    int timer;
+    int score;
+    static const int noBricks = 30;
     static const int DELAY = 10;
     static const int BOTTOM_EDGE = 400;
     Ball *ball;
     Paddle *paddle;
-    Brick *bricks[NUM_BRICKS];
-    bool gameOver;
+    Brick *bricks[noBricks];
+    bool gameEnded;
     bool gameWon;
     bool gameStarted;
     bool paused;
