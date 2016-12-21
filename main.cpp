a4a08ec1ae4866462ae4f41a6812841cb9d10138
arnav-s <sharma55@wisc.edu>
2016-12-20 22:42:54 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
 #include <QApplication>
 #include "Game.hpp"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);  
+    QApplication app(argc, argv);
+
+    std::cout << argc << " " << *argv << std::endl;
 
     Game window;
 
     window.resize(300, 400);
-    window.setWindowTitle("Breakout");
+    window.setWindowTitle("Brick Breaker");
     window.show();
 
     return app.exec();
 }
-
