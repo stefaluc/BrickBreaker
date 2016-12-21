5a090066a3f8757a4527e29b37413e20b1e43938
arnav-s <sharma55@wisc.edu>
2016-12-20 22:40:46 -0600

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,22 +1,23 @@
 #include <QPainter>
 #include <QApplication>
-#include "breakout.h"
+#include "Game.hpp"
+#include <iostream>
 
 Game::Game(QWidget *parent) : QWidget(parent) {
     x = 0;
-    gameOver = false;
+    gameEnded = false;
     gameWon = false;
     paused = false;
     gameStarted = false;
     ball = new Ball();
     paddle = new Paddle();
-
-    int k = 0;
+    score = 0;
+    int numBricks = 0;
 
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 6; j++) {
-            bricks[k] = new Brick((j*40) + 30, (i*10) + 50);
-            k++; 
+            bricks[numBricks] = new Brick((j*60), (i*22.5));
+            numBricks++;
         }
     } 
 }
@@ -25,103 +26,192 @@ Game::~Game() {
     delete ball;
     delete paddle;
 
-    for (int i=0; i<NUM_BRICKS; i++) {
+    for (int i=0; i<noBricks; i++) {
         delete bricks[i];
     }
 }
 
-void Game::paintEvent(QPaintEvent *e) {
+void Game::paintEvent(QPaintEvent *evnt) {
 
-    Q_UNUSED(e);
+    Q_UNUSED(evnt);
 
     QPainter painter(this);
 
-    if (gameOver) {
-        finishGame(&painter, "Game lost");
+    if (gameEnded) {
+        endGame(&painter, "You Lost");
     } else if(gameWon) {
-        finishGame(&painter, "Victory");
-    } else {
+        endGame(&painter, "You Won");
+    }
+
+    else {
         drawObjects(&painter);
     }
 }
 
-void Game::finishGame(QPainter *painter, QString message) {
-
-    QFont font("Courier", 15, QFont::DemiBold);
-    QFontMetrics fm(font);
-    int textWidth = fm.width(message);
-
-    painter->setFont(font);
-    int h = height();
-    int w = width();
-
-    painter->translate(QPoint(w/2, h/2));
-    painter->drawText(-textWidth/2, 0, message);
-}
 
 void Game::drawObjects(QPainter *painter) {
-    painter->drawImage(ball->getRect(), ball->getImage());
-    painter->drawImage(paddle->getRect(), paddle->getImage());
-
-    for (int i=0; i<NUM_BRICKS; i++) {
+    painter->drawImage(ball->getRectangle(), ball->getImage());
+    painter->drawImage(paddle->getRectangle(), paddle->getImage());
+    QString msg= "Score: "+QString::number(score);
+    painter->drawText(1,399,msg);
+    for (int i=0; i<noBricks; i++) {
         if (!bricks[i]->isDestroyed()) {
-            painter->drawImage(bricks[i]->getRect(), bricks[i]->getImage());
+            painter->drawImage(bricks[i]->getRectangle(), bricks[i]->getImage());
         }
     } 
 }
 
-void Game::timerEvent(QTimerEvent *e) {
-    Q_UNUSED(e);
+void Game::timerEvent(QTimerEvent *evnt) {
+    Q_UNUSED(evnt);
 
     moveObjects();
-    checkCollision();
+    Physics();
     repaint();
 }
 
 void Game::moveObjects() {
-    ball->autoMove();
+    ball->ballMove();
     paddle->move();
 }
 
-void Game::keyReleaseEvent(QKeyEvent *e) {
-    int dx = 0;
+void Game::keyReleaseEvent(QKeyEvent *evnt) {
+    int vx = 0;
 
-    switch (e->key()) {
+    switch (evnt->key()) {
         case Qt::Key_Left:
-            dx = 0;
-            paddle->setDx(dx);
+            paddle->setvx(vx);
             break;
 
         case Qt::Key_Right:
-            dx = 0;
-            paddle->setDx(dx);
+            paddle->setvx(vx);
             break;
     }
 }
 
-void Game::keyPressEvent(QKeyEvent *e) {
-    int dx = 0;
+void Game::keyPressEvent(QKeyEvent *evnt) {
+    int vx = 0;
 
-    switch (e->key()) {
+    switch (evnt->key()) {
         case Qt::Key_Left:
-            dx = -1;
-            paddle->setDx(dx);
+            vx = -1;
+            paddle->setvx(vx);
             break;
+
         case Qt::Key_Right:
-            dx = 1;
-            paddle->setDx(dx);
+            vx = 1;
+            paddle->setvx(vx);
             break;
-        case Qt::Key_P:
-            pauseGame();
-            break;
+
         case Qt::Key_Space:
-            startGame();
+            if(!gameStarted)
+                startGame();
+            else{
+                pauseGame();
+            }
             break;
+
         case Qt::Key_Escape:
             qApp->exit();
             break;
+
         default:
-            QWidget::keyPressEvent(e);
+            QWidget::keyPressEvent(evnt);
+    }
+}
+
+void Game::Physics() {
+
+    if (ball->getRectangle().bottom() > BOTTOM_EDGE) {
+        endGame();
+    }
+    score=0;
+    for (int i=0; i<noBricks; i++) {
+        if (bricks[i]->isDestroyed()) {
+            score++;
+        }
+        if (score == noBricks) {
+            Win();
+        }
+    }
+
+
+
+    if ((ball->getRectangle()).intersects(paddle->getRectangle()))
+    {
+
+        int paddle_Left_Pos = paddle->getRectangle().left();
+        int ball_Left_Pos = ball->getRectangle().left();
+
+        int first = paddle_Left_Pos + 8;
+           int second = paddle_Left_Pos + 16;
+           int third = paddle_Left_Pos + 24;
+           int fourth = paddle_Left_Pos + 32;
+
+           if (ball_Left_Pos < first)
+           {
+             ball->setX(-1);
+             ball->setY(-1);
+           }
+
+           if (ball_Left_Pos >= first && ball_Left_Pos < second)
+           {
+             ball->setX(-1);
+             ball->setY(-1*ball->getY());
+           }
+
+           if (ball_Left_Pos >= second && ball_Left_Pos < third)
+           {
+              ball->setX(0);
+              ball->setY(-1);
+           }
+
+           if (ball_Left_Pos >= third && ball_Left_Pos < fourth)
+           {
+              ball->setX(1);
+              ball->setY(-1*ball->getY());
+           }
+
+           if (ball_Left_Pos > fourth)
+           {
+             ball->setX(1);
+             ball->setY(-1);
+           }
+         }
+
+    for (int i=0; i<noBricks; i++)
+    {
+        if ((ball->getRectangle()).intersects(bricks[i]->getRectangle())) {
+
+            int ballLeft = ball->getRectangle().left();
+            int ballHeight = ball->getRectangle().height();
+            int ballWidth = ball->getRectangle().width();
+            int ballTop = ball->getRectangle().top();
+
+            QPoint pointRight(ballLeft + ballWidth + 1, ballTop);
+            QPoint pointLeft(ballLeft - 1, ballTop);
+            QPoint pointTop(ballLeft, ballTop -1);
+            QPoint pointBottom(ballLeft, ballTop + ballHeight + 1);
+
+            if (!bricks[i]->isDestroyed()) {
+                if(bricks[i]->getRectangle().contains(pointRight)) {
+                    ball->setX(-1);
+                }
+
+                else if(bricks[i]->getRectangle().contains(pointLeft)) {
+                    ball->setX(1);
+                }
+
+                if(bricks[i]->getRectangle().contains(pointTop)) {
+                    ball->setY(1);
+                }
+
+                else if(bricks[i]->getRectangle().contains(pointBottom)) {
+                    ball->setY(-1);
+                }
+
+                bricks[i]->setDestroyed(true);
+            }
+        }
     }
 }
 
@@ -130,120 +220,49 @@ void Game::startGame() {
         ball->resetState();
         paddle->resetState();
 
-        for (int i=0; i<NUM_BRICKS; i++) {
+        for (int i=0; i<noBricks; i++) {
             bricks[i]->setDestroyed(false);
         }
 
-        gameOver = false; 
+        gameEnded = false;
         gameWon = false; 
         gameStarted = true;
-        timerId = startTimer(DELAY);
+        timer = startTimer(DELAY);
     } 
 }
 
 void Game::pauseGame() {
     if (paused) {
-        timerId = startTimer(DELAY);
+        timer = startTimer(DELAY);
         paused = false;
     } else {
         paused = true;
-        killTimer(timerId); 
+        killTimer(timer);
     } 
 }
 
-void Game::stopGame() {
-    killTimer(timerId);
-    gameOver = true; 
+void Game::endGame() {
+    killTimer(timer);
+    gameEnded = true;
     gameStarted = false;
 }
+void Game::endGame(QPainter *painter, QString msg) {
 
-void Game::victory() {
-    killTimer(timerId);
+    QFont font("Arial", 12, QFont::Bold);
+    QFontMetrics f(font);
+    int textWidth = f.width(msg);
+
+    painter->setFont(font);
+    int h = height();
+    int w = width();
+
+    painter->translate(QPoint(w/2, h/2));
+    painter->drawText(-textWidth/2, 0, msg);
+}
+
+void Game::Win() {
+    killTimer(timer);
     gameWon = true;
     gameStarted = false;
 }
 
-void Game::checkCollision() {
-    if (ball->getRect().bottom() > BOTTOM_EDGE) {
-        stopGame();
-    }
-
-    for (int i=0, j=0; i<NUM_BRICKS; i++) {
-        if (bricks[i]->isDestroyed()) {
-            j++;
-        }
-        if (j == NUM_BRICKS) {
-            victory();
-        }
-    }
-
-    if ((ball->getRect()).intersects(paddle->getRect())) {
-        int paddleLPos = paddle->getRect().left();
-        int ballLPos = ball->getRect().left(); 
-
-        int first = paddleLPos + 8;
-        int second = paddleLPos + 16;
-        int third = paddleLPos + 24;
-        int fourth = paddleLPos + 32;
-
-        if (ballLPos < first) {
-            ball->setXDir(-1);
-            ball->setYDir(-1);
-        }
-
-        if (ballLPos >= first && ballLPos < second) {
-            ball->setXDir(-1);
-            ball->setYDir(-1*ball->getYDir());
-        }
-
-        if (ballLPos >= second && ballLPos < third) {
-            ball->setXDir(0);
-            ball->setYDir(-1);
-        }
-
-        if (ballLPos >= third && ballLPos < fourth) {
-            ball->setXDir(1);
-            ball->setYDir(-1*ball->getYDir());
-        }
-
-        if (ballLPos > fourth) {
-            ball->setXDir(1);
-            ball->setYDir(-1);
-        }
-    } 
-
-    for (int i=0; i<NUM_BRICKS; i++) {
-        if ((ball->getRect()).intersects(bricks[i]->getRect())) {
-
-            int ballLeft = ball->getRect().left();
-            int ballHeight = ball->getRect().height(); 
-            int ballWidth = ball->getRect().width();
-            int ballTop = ball->getRect().top();
-
-            QPoint pointRight(ballLeft + ballWidth + 1, ballTop);
-            QPoint pointLeft(ballLeft - 1, ballTop);
-            QPoint pointTop(ballLeft, ballTop -1);
-            QPoint pointBottom(ballLeft, ballTop + ballHeight + 1);
-
-            if (!bricks[i]->isDestroyed()) {
-                if(bricks[i]->getRect().contains(pointRight)) {
-                    ball->setXDir(-1);
-                }
-
-                else if(bricks[i]->getRect().contains(pointLeft)) {
-                    ball->setXDir(1);
-                }
-
-                if(bricks[i]->getRect().contains(pointTop)) {
-                    ball->setYDir(1);
-                }
-
-                else if(bricks[i]->getRect().contains(pointBottom)) {
-                    ball->setYDir(-1);
-                }
-
-                bricks[i]->setDestroyed(true);
-            }
-        }
-    }
-}
